22d4e2c14e0392e6a7dd522edf26be4a504ff363
Mateus Sereno <mateus.sereno@gmail.com>
2023-05-29 09:35:40 -0300

    UI

--- compile.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set QGIS_PATH=D:\Program Files\QGIS 3.16\bin
-call "%QGIS_PATH%\o4w_env.bat"
-call "%QGIS_PATH%\qt5_env.bat"
-call "%QGIS_PATH%\py3_env.bat"
-
-@echo on
-pyrcc5 -o resources.py resources.qrc
